55e736155d405fc16ec16a38436d7d0dc7118008
genuinewords <fakhirsoft@gmail.com>
2026-04-15 20:52:37 -0700

    4

--- theme/i18n/types.ts
+++ theme/i18n/types.ts
@@ -128,6 +128,9 @@ export interface Translations {
 
 export interface NavLabels {
   home: string;
+  linearCalc: string;
+  quadCalc: string;
+  multiCalc: string;
   pearsonCorrelation: string;
   grubbsTest: string;
   assumptionsChecker: string;
