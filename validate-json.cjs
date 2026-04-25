2ef2444742cfddca696677be88b74e76569a70fa
genuinewords <fakhirsoft@gmail.com>
2026-04-25 05:14:07 -0700

    Fix broken JSON syntax in shared locale files

--- validate-json.cjs
+++ validate-json.cjs
@@ -1,10 +1,19 @@
 const fs = require('fs');
-const locales = ['hi','es','ru','fr','de','it','pt','bn','ja','ko','ms','pl','id','ar','bg','tr','sv'];
-for (const loc of locales) {
+const dir = 'C:/Users/Z/Documents/GitHub/Regression-Equation-Calculator1/src/i18n';
+fs.readdirSync(dir).filter(f => f.endsWith('.json') && f !== 'en.json').forEach(f => {
   try {
-    JSON.parse(fs.readFileSync('src/i18n/' + loc + '.json', 'utf8'));
-    console.log(loc + '.json: VALID');
+    JSON.parse(fs.readFileSync(dir + '/' + f, 'utf8'));
+    console.log('OK ' + f);
   } catch (e) {
-    console.log(loc + '.json: INVALID - ' + e.message);
+    console.log('BROKEN ' + f + ': ' + e.message.split('\n')[0]);
   }
-}
+});
+const sdir = dir + '/shared';
+fs.readdirSync(sdir).filter(f => f.endsWith('.json')).forEach(f => {
+  try {
+    JSON.parse(fs.readFileSync(sdir + '/' + f, 'utf8'));
+    console.log('OK shared/' + f);
+  } catch (e) {
+    console.log('BROKEN shared/' + f + ': ' + e.message.split('\n')[0]);
+  }
+});
