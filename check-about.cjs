3e6cd4c887f1778bde1e27b5b4dd3f5a926e5d50
genuinewords <fakhirsoft@gmail.com>
2026-04-27 04:55:10 -0700

    37

--- check-about.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const en = JSON.parse(fs.readFileSync('src/i18n/en.json', 'utf8'));
-console.log('en.json about keys:', Object.keys(en.about));
-
-const enAbout = en.about;
-const missingFromEn = ['mission', 'missionText', 'values', 'valuesText', 'team', 'teamText'];
-for (const k of missingFromEn) {
-  console.log(k, 'in en.json:', enAbout[k] !== undefined ? 'YES' : 'NO');
-}
